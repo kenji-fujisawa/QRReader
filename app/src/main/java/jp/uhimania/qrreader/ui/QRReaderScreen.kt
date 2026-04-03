667eecd12a3c4bde5adc14410fc7aa1eabec7aba
uhimania <uhimania@uhimanianoMacBook-Air.local>
2026-04-03 14:18:58 +0900

    ViewModel に変更

--- app/src/main/java/jp/uhimania/qrreader/ui/QRReaderScreen.kt
+++ app/src/main/java/jp/uhimania/qrreader/ui/QRReaderScreen.kt
@@ -2,7 +2,6 @@ package jp.uhimania.qrreader.ui
 
 import android.annotation.SuppressLint
 import android.content.ClipData
-import android.graphics.Rect
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
@@ -12,10 +11,9 @@ import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.SnackbarResult
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.platform.LocalClipboard
@@ -26,19 +24,21 @@ import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.platform.toClipEntry
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.viewmodel.compose.viewModel
 import jp.uhimania.qrreader.R
-import java.net.URL
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter", "ConfigurationScreenWidthHeight")
 @Composable
 fun QRReaderScreen(
     modifier: Modifier = Modifier,
+    viewModel: QRReaderViewModel = viewModel(factory = QRReaderViewModel.Factory),
     snackbarHostState: SnackbarHostState = remember { SnackbarHostState() }
 ) {
     Scaffold(
         modifier = modifier.fillMaxSize(),
         snackbarHost = { SnackbarHost(snackbarHostState) }
     ) {
+        val uiState by viewModel.uiState.collectAsState()
         val density = LocalDensity.current
         val configuration = LocalConfiguration.current
         val screenSize = with(density) {
@@ -48,10 +48,6 @@ fun QRReaderScreen(
             )
         }
 
-        var decodedText by remember { mutableStateOf<String?>(null) }
-        var barcodeRect by remember { mutableStateOf<Rect?>(null) }
-        var imageSize by remember { mutableStateOf(Size.Zero) }
-
         val context = LocalContext.current
         val lifecycleOwner = LocalLifecycleOwner.current
         val cameraProvider = remember {
@@ -60,11 +56,10 @@ fun QRReaderScreen(
                 lifecycleOwner = lifecycleOwner,
                 callback = { barcodes, size ->
                     barcodes.firstOrNull()?.displayValue?.let {
-                        if (it != decodedText)
-                            decodedText = it
+                        viewModel.updateDecodedText(it)
                     }
-                    barcodeRect = barcodes.firstOrNull()?.boundingBox
-                    imageSize = size
+                    viewModel.updateBarcodeRect(barcodes.firstOrNull()?.boundingBox)
+                    viewModel.updateImageSize(size)
                 }
             )
         }
@@ -76,29 +71,29 @@ fun QRReaderScreen(
                     cameraProvider.bind(it)
                 }
             )
-            barcodeRect?.let {
+            uiState.barCodeRect?.let {
                 BarcodeMarker(
                     barcodeRect = it,
                     screenSize = screenSize,
-                    imageSize = imageSize
+                    imageSize = uiState.imageSize
                 )
             }
         }
 
-        decodedText?.let {
+        uiState.decodedText?.let {
             val urlHandler = LocalUriHandler.current
             val clipboard = LocalClipboard.current
-            LaunchedEffect(snackbarHostState, decodedText) {
+            LaunchedEffect(snackbarHostState, it) {
                 val open = context.getString(R.string.action_label_open)
                 val copy = context.getString(R.string.action_label_copy)
                 val result = snackbarHostState.showSnackbar(
                     message = it,
-                    actionLabel = if (it.isURL()) open else copy,
+                    actionLabel = if (uiState.isUrl) open else copy,
                     duration = SnackbarDuration.Long
                 )
                 when (result) {
                     SnackbarResult.ActionPerformed -> {
-                        if (it.isURL()) {
+                        if (uiState.isUrl) {
                             urlHandler.openUri(it)
                         } else {
                             val data = ClipData.newPlainText(it, it)
@@ -106,19 +101,10 @@ fun QRReaderScreen(
                         }
                     }
                     SnackbarResult.Dismissed -> {
-                        decodedText = null
+                        viewModel.updateDecodedText(null)
                     }
                 }
             }
         }
-    }
-}
-
-private fun String.isURL(): Boolean {
-    return try {
-        URL(this)
-        true
-    } catch (_: Exception) {
-        false
     }
 }
