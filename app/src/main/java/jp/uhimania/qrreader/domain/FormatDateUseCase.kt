fba50c6b28fead4f014692021edc4a58c4da744e
uhimania <uhimania@uhimanianoMacBook-Air.local>
2026-04-13 15:15:44 +0900

    前日が一ヶ月前表示になってしまうことがあったのを修正

--- app/src/main/java/jp/uhimania/qrreader/domain/FormatDateUseCase.kt
+++ app/src/main/java/jp/uhimania/qrreader/domain/FormatDateUseCase.kt
@@ -4,6 +4,7 @@ import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Date
 import java.util.Locale
+import kotlin.math.abs
 
 sealed class DateFormat {
     object Today : DateFormat()
@@ -15,12 +16,14 @@ sealed class DateFormat {
 class FormatDateUseCase {
     operator fun invoke(date: Date): DateFormat {
         val now = Date()
+        val diff = abs(now.time - date.time)
         if (date.year() == now.year() && date.month() == now.month() && date.day() == now.day()) {
             return DateFormat.Today
-        } else if (date.year() == now.year() && date.month() == now.month()) {
-            return DateFormat.DaysAgo(now.day() - date.day())
-        } else if (date.year() == now.year()) {
-            return DateFormat.MonthsAgo(now.month() - date.month())
+        } else if (diff <= 30L * 24 * 60 * 60 * 1000) {
+            return DateFormat.DaysAgo(now.dayOfYear() - date.dayOfYear())
+        } else if (diff <= 365L * 24 * 60 * 60 * 1000) {
+            val months = if (date.month() > now.month()) now.month() + 12 else now.month()
+            return DateFormat.MonthsAgo(months - date.month())
         } else {
             val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
             return DateFormat.Date(formatter.format(date))
@@ -44,4 +47,10 @@ fun Date.day(): Int {
     val calendar = Calendar.getInstance()
     calendar.time = this
     return calendar.get(Calendar.DAY_OF_MONTH)
+}
+
+fun Date.dayOfYear(): Int {
+    val calendar = Calendar.getInstance()
+    calendar.time = this
+    return calendar.get(Calendar.DAY_OF_YEAR)
 }
--- app/src/test/java/jp/uhimania/qrreader/domain/FormatDateUseCaseTest.kt
+++ app/src/test/java/jp/uhimania/qrreader/domain/FormatDateUseCaseTest.kt
@@ -11,14 +11,31 @@ class FormatDateUseCaseTest {
     fun testInvoke() {
         val useCase = FormatDateUseCase()
 
-        val calendar = Calendar.getInstance()
+        var calendar = Calendar.getInstance()
         assertEquals(DateFormat.Today, useCase(calendar.time))
 
-        calendar.add(Calendar.DAY_OF_MONTH, -1)
+        val hours = calendar.get(Calendar.HOUR_OF_DAY) + 1
+        calendar.add(Calendar.HOUR_OF_DAY, hours * -1)
         assertEquals(DateFormat.DaysAgo(1), useCase(calendar.time))
+
+        calendar = Calendar.getInstance()
+
+        val days = calendar.get(Calendar.DAY_OF_MONTH)
+        calendar.add(Calendar.DAY_OF_MONTH, days * -1)
+        assertEquals(DateFormat.DaysAgo(days), useCase(calendar.time))
+
+        calendar = Calendar.getInstance()
 
         calendar.add(Calendar.MONTH, -1)
         assertEquals(DateFormat.MonthsAgo(1), useCase(calendar.time))
+
+        calendar = Calendar.getInstance()
+
+        val months = calendar.get(Calendar.MONTH) + 1
+        calendar.add(Calendar.MONTH, months * -1)
+        assertEquals(DateFormat.MonthsAgo(months), useCase(calendar.time))
+
+        calendar = Calendar.getInstance()
 
         calendar.add(Calendar.YEAR, -1)
         val formatter = SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.getDefault())
